46e06d50a43528d1a7ebccee69a868aa29eb51c4
KhoaSu09 <62194647+KhoaSu09@users.noreply.github.com>
2026-03-25 20:39:06 +0700

    Add files via upload

--- cart.js
+++ cart.js
@@ -79,6 +79,7 @@ const CART_KEY = 'vl_cart';
 const ORDERS_KEY = 'vl_orders_v1';
 
 function formatPriceGlobal(price) {
+  if (!price || price === 0) return 'Liên hệ';
   return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(price);
 }
 function escapeHtmlGlobal(s) {
